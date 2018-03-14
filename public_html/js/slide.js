4808541d1d0119c61ed734e117e7693627cb8904
Ben <bliston@eginity.com>
2018-03-14 14:52:41 -0400

    spelling errors

--- public_html/js/slide.js
+++ public_html/js/slide.js
@@ -1,19 +1,19 @@
 var imageCount = 1;
-var total = 10;
+var total = 3;
 
 function photo(x) {
 	var image = document.getElementById('image');
 	imageCount = imageCount + x;
 	if(imageCount > total){imageCount = 1;}
 	if(imageCount < 1){imageCount = total;}	
-	image.src = "../slideshow/img"+ imageCount +".jpg";
+	image.src = "slideshow/img"+ imageCount +".jpg";
 	clearInterval(time); 								// clear interval stops the set interval.
 	time =  window.setInterval(function photoA() { 		// givig the value of time the samfunction below starts the loop 
 	var image = document.getElementById('image');
 	imageCount = imageCount + 1;
 	if(imageCount > total){imageCount = 1;}
 	if(imageCount < 1){imageCount = total;}	
-	image.src = "../slideshow/img"+ imageCount +".jpg";
+	image.src = "slideshow/img"+ imageCount +".jpg";
 	},3000);
 	}
  
@@ -22,7 +22,7 @@ var time = window.setInterval(function photoA() {    // just addign the sunction
 	imageCount = imageCount + 1;
 	if(imageCount > total){imageCount = 1;}
 	if(imageCount < 1){imageCount = total;}	
-	image.src = "../slideshow/img"+ imageCount +".jpg";
+	image.src = "slideshow/img"+ imageCount +".jpg";
 	},3000);
 
 
